b42776a2b61aabbbf9e8368af08164931444e563
Samuca23 <samuelchiodini@hotmail.com>
2024-01-11 14:59:28 -0500

    front completo

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,123 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+
+  const produto = {
+    codigo : 0,
+    nome : '',
+    marca : ''
+  }
+
+  const [produtos, setProdutos] = useState([]);
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value});
+  }
+
+  const cadastrar = () => {
+    fetch("http://localhost:8080/cadastrar", {
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+
+      if (retorno_convertido.mensagem !== undefined) {
+        alert(retorno_convertido.mensagem);
+      } else {
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso!');
+        limparFormulario();
+      }
+
+    })
+  }
+
+  const alterar = () => {
+    fetch("http://localhost:8080/alterar", {
+      method:'put',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+
+      if (retorno_convertido.mensagem !== undefined) {
+        alert(retorno_convertido.mensagem);
+      } else {
+        alert('Produto alterado com sucesso!');
+        let vetorTemp = [...produtos];
+
+        let indice = vetorTemp.findIndex((p) => {
+          return p.codigo === objProduto.codigo;
+        });
+
+        vetorTemp[indice] = objProduto;
+      
+        setProdutos(vetorTemp);
+        limparFormulario();
+      }
+
+    })
+  }
+
+  const remover = () => {
+    fetch('http://localhost:8080/excluir/'+objProduto.codigo, {
+      method:'delete',
+      headers:{
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      alert(retorno_convertido.mensagem);
+
+      let vetorTemp = [...produtos];
+
+      let indice = vetorTemp.findIndex((p) => {
+        return p.codigo === objProduto.codigo;
+      });
+
+      vetorTemp.splice(indice, 1);
+      
+      setProdutos(vetorTemp);
+      limparFormulario();
+    })
+  }
+
+  const limparFormulario = () => {
+    setObjProduto(produto);
+    setBtnCadastrar(true);
+  }
+
+  const selecionarProduto = (indice) => {
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
   return (
     <div>
-      <Formulario/>
-      <Tabela/>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} alterar={alterar} remover={remover}/>
+      <Tabela vetor={produtos} selecionar={selecionarProduto}/>
     </div>
   );
 }
--- front/src/Formulario.js
+++ front/src/Formulario.js
@@ -1,13 +1,20 @@
-function Formulario() {
+function Formulario({botao, eventoTeclado, cadastrar, obj, cancelar, alterar, remover}) {
     return(
         <form>
-            <input type="text" placeholder="Nome" className="form-control" />
-            <input type="text" placeholder="Marca" className="form-control"/>
+            <input type="text" onChange={eventoTeclado} name="nome" placeholder="Nome" className="form-control" value={obj.nome}/>
+            <input type="text" onChange={eventoTeclado} name="marca" placeholder="Marca" className="form-control" value={obj.marca}/>
 
-            <input type="button" value="Cadastrar" className="btn btn-primary"/>
-            <input type="button" value="Alterar" className="btn btn-warning"/>
-            <input type="button" value="Remover" className="btn btn-danger"/>
-            <input type="button" value="Cancelar" className="btn btn-secondary"/>
+        {
+            botao
+            ?
+            <input type="button" value="Cadastrar" onClick={cadastrar} className="btn btn-primary"/>
+            :
+            <div>
+                <input type="button" value="Alterar" className="btn btn-warning" onClick={alterar} />
+                <input type="button" value="Remover" className="btn btn-danger" onClick={remover} />
+                <input type="button" value="Cancelar" className="btn btn-secondary" onClick={cancelar} />
+            </div>
+        }
         </form>
     )
 }
