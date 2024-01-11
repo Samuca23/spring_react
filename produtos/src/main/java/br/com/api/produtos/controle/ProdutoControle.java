100718e5410ddcafc2e62d2aa262f76395280e0c
Samuca23 <samuelchiodini@hotmail.com>
2024-01-11 09:15:04 -0500

    cadastro de produto

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -3,7 +3,10 @@ package br.com.api.produtos.controle;
 import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,9 +20,8 @@ public class ProdutoControle {
         return ps.listar();
     }
 
-    @GetMapping("/")
-    public String rota() {
-        return "Api de produtos";
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
+        return ps.cadastrar(pm);
     }
-
 }
