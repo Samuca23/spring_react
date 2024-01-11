152342f070810debebaac38eba2c74561cfa1949
Samuca23 <samuelchiodini@hotmail.com>
2024-01-11 09:25:40 -0500

    alterar produto

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,10 +4,7 @@ import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProdutoControle {
@@ -22,6 +19,11 @@ public class ProdutoControle {
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
-        return ps.cadastrar(pm);
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
+        return ps.cadastrarAlterar(pm, "alterar");
     }
 }
